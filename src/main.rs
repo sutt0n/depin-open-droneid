f1ca7cbf9c940cb57b61b74cd7187af57d96cf57
Joseph Sutton <ebwebdev@gmail.com>
2024-05-08 18:39:37 -0500

    feat: cleanup

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,22 @@ fn enable_monitor_mode(device: &str) -> Result<(), String> {
     Ok(())
 }
 
+fn disable_monitor_mode(device: &str) -> Result<(), String> {
+    // Disable monitoring mode using airmon-ng
+    let stop_mon = SysCommand::new("sudo")
+        .args(["airmon-ng", "stop", device])
+        .output()
+        .expect("failed to execute process");
+
+    if stop_mon.status.success() {
+        println!("Monitoring mode disabled on {}", device);
+    } else {
+        return Err("Failed to disable monitoring mode".to_string());
+    }
+
+    Ok(())
+}
+
 fn main() {
     let matches = Command::new("Drone ID Scanner")
         .version("1.0")
@@ -66,4 +82,8 @@ fn main() {
     while let Ok(packet) = cap.as_mut().unwrap().next_packet() {
         println!("received packet: {:?}", packet);
     }
+
+    println!("Exiting...");
+
+    disable_monitor_mode(device_name).unwrap_or_else(|err| eprintln!("Error: {}", err));
 }
