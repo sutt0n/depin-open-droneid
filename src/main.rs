0597beefcfebfeec19fb0fdc774e75a99e748656
Joseph Sutton <ebwebdev@gmail.com>
2024-05-16 10:28:21 -0500

    bluez

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,24 @@
-use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter};
-use btleplug::platform::Manager;
+//! Example to log Bluetooth events, including duplicate manufacturer-specific advertisement data.
+
+use bluez_async::{BluetoothSession, DiscoveryFilter};
 use futures::stream::StreamExt;
-use serde::Serialize;
-use std::collections::HashMap;
-use tokio::time::Duration;
-
-#[derive(Serialize)]
-struct DeviceProperties {
-    local_name: Option<String>,
-    rssi: Option<i16>,
-    address: String,
-    manufacturer_data: Option<HashMap<u16, String>>, // Decode as String
-}
-
-fn decode_manufacturer_data(data: HashMap<u16, Vec<u8>>) -> HashMap<u16, String> {
-    data.into_iter()
-        .map(|(key, value)| {
-            let decoded = String::from_utf8_lossy(&value).to_string();
-            (key, decoded)
-        })
-        .collect()
-}
 
 #[tokio::main]
-async fn main() {
-    // Create a new manager
-    let manager = Manager::new().await.unwrap();
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
 
-    // Get the first adapter
-    let adapters = manager.adapters().await.unwrap();
-    let central = adapters.into_iter().nth(0).unwrap();
+    let (_, session) = BluetoothSession::new().await?;
+    let mut events = session.event_stream().await?;
+    session
+        .start_discovery_with_filter(&DiscoveryFilter {
+            duplicate_data: Some(true),
+            ..DiscoveryFilter::default()
+        })
+        .await?;
 
-    // Start scanning for devices
-    central.start_scan(ScanFilter::default()).await.unwrap();
-    tokio::time::sleep(Duration::from_secs(2)).await;
-
-    // Continuously read BLE advertisements
-    let mut events = central.events().await.unwrap();
+    println!("Events:");
     while let Some(event) = events.next().await {
-        match event {
-            btleplug::api::CentralEvent::DeviceDiscovered(id)
-            | btleplug::api::CentralEvent::ManufacturerDataAdvertisement { id, .. } => {
-                let peripheral = central.peripheral(&id).await.unwrap();
-                let properties = peripheral.properties().await.unwrap();
-                if let Some(properties) = properties {
-                    let decoded_manufacturer_data = if !properties.manufacturer_data.is_empty() {
-                        Some(decode_manufacturer_data(properties.manufacturer_data.clone()))
-                    } else {
-                        None
-                    };
-
-                    println!("Service Data: {:?}", properties);
-                    let device_props = DeviceProperties {
-                        local_name: properties.local_name,
-                        rssi: properties.rssi,
-                        address: peripheral.address().to_string(),
-                        manufacturer_data: decoded_manufacturer_data,
-                    };
-                    let json = serde_json::to_string(&device_props).unwrap();
-                    println!("{}", json);
-                }
-            }
-            _ => {}
-        }
+        println!("{:?}", event);
     }
+
+    Ok(())
 }
