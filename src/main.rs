3662ed1973b0522a7efa08266c5ab836731f6966
Joseph Sutton <ebwebdev@gmail.com>
2024-05-17 09:37:54 -0500

    filter service data through discovery

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
         .await?;
 
     println!("Events:");
-    while let Some(event) = events.next().await {
-        println!("{:?}", event);
-    }
+    // while let Some(event) = events.next().await {
+    //     println!("{:?}", event);
+    // }
+
+    tokio::spawn(async move {
+        while let Some(event) = events.next().await {
+            match event {
+                bluez_async::BluetoothEvent::Device{id,event} => {
+                    println!("Device: {:?} {:?}",id,event);
+                    match event {
+                        bluez_async::DeviceEvent::ServiceData { service_data } => {
+                            // get first value of service data
+                            let first_value = service_data.values().next().unwrap();
+                            // to &[u8]
+                            let data = first_value.as_slice();
+
+                            if data.len() < 24 {
+                                continue;
+                            }
+
+                            println!("Service Data: {:?}", service_data);
+
+                        }
+                        _ => {}
+                    }
+                }
+                bluez_async::BluetoothEvent::Adapter { id, event } => todo!(),
+                bluez_async::BluetoothEvent::Characteristic { id, event } => todo!(), }
+        }
+    }).await?;
 
     Ok(())
 }
