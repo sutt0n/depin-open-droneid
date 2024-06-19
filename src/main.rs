59b1b1cdc7c4d573e0d9e1f335d6c0cf3b0a66c9
Joseph Sutton <ebwebdev@gmail.com>
2024-06-19 15:37:16 -0500

    check: wifi

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use axum::Router;
 use bluez_async::{BluetoothSession, DeviceId, DiscoveryFilter};
 use futures::stream::StreamExt;
 use models::DroneDto;
+use pcap::{Capture, Device};
 use routes::insert_drone;
-use sqlx::postgres::PgPoolOptions;
 
 mod bluetooth;
 mod drone;
@@ -17,13 +17,16 @@ mod parsers;
 mod router;
 mod routes;
 mod templates;
+mod wifi;
 
+use crate::wifi::{disable_monitor_mode, enable_monitor_mode};
 use crate::bluetooth::handle_bluetooth_event;
 use crate::drone::Drone;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let device_name = "hci0";
+    let wifi_device_name = "wlx98254aed7e95";
 
     let mut drones: HashMap<DeviceId, Drone> = HashMap::new();
 
@@ -39,6 +42,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("Failed to run migrations");
 
     let (router, tx) = router::init_router(sqlx_connection.clone());
+
+
+    let wifi_task = tokio::spawn(async move {
+        if let Err(e) = enable_monitor_mode(wifi_device_name) {
+            eprintln!("Error: {}", e);
+            return;
+        }
+
+        println!("Using device: {}", wifi_device_name);
+
+        let mut cap = Capture::from_device(wifi_device_name.as_str()).unwrap()
+            .promisc(true)
+            .open();
+
+        if let Err(e) = cap {
+            eprintln!("error opening device \"{}\": {}", wifi_device_name, e);
+            let devices = Device::list().unwrap();
+            let device_names = devices.iter().map(|d| d.name.clone()).collect::<Vec<String>>();
+
+            eprintln!("available devices: {:?}", device_names);
+            return;
+        }
+
+        let mut cap = cap.unwrap().setnonblock().unwrap();
+
+        cap.for_each(None, |packet| {
+            println!("packet {:?}", packet);
+        });
+    });
 
     // Spawn a task to handle bluetooth events
     let bt_task = tokio::spawn(async move {
---
+++ src/wifi.rs
@@ -0,0 +1,43 @@
+use std::process::Command as SysCommand;
+
+pub fn enable_monitor_mode(device: &str) -> Result<(), String> {
+    // Check if the device is already in monitoring mode
+    let check_mode = SysCommand::new("iwconfig")
+        .arg(device)
+        .output()
+        .expect("failed to execute process");
+
+    let output = String::from_utf8_lossy(&check_mode.stdout);
+
+    if !output.contains("Monitor mode enabled") {
+        // Enable monitoring mode using airmon-ng
+        let start_mon = SysCommand::new("sudo")
+            .args(["airmon-ng", "start", device])
+            .output()
+            .expect("failed to execute process");
+
+        if start_mon.status.success() {
+            println!("Monitoring mode enabled on {}", device);
+        } else {
+            return Err("Failed to enable monitoring mode".to_string());
+        }
+    }
+
+    Ok(())
+}
+
+pub fn disable_monitor_mode(device: &str) -> Result<(), String> {
+    // Disable monitoring mode using airmon-ng
+    let stop_mon = SysCommand::new("sudo")
+        .args(["airmon-ng", "stop", device])
+        .output()
+        .expect("failed to execute process");
+
+    if stop_mon.status.success() {
+        println!("Monitoring mode disabled on {}", device);
+    } else {
+        return Err("Failed to disable monitoring mode".to_string());
+    }
+
+    Ok(())
+}
