9936d05d1e2c89dc9d987c47fa12cb06ada68333
Jojo Sutton <sutt0n@users.noreply.github.com>
2024-07-05 18:27:42 -0500

    refactor: use derive builder, and parse wifi messages (#3)

--- src/bluetooth/task.rs
+++ src/bluetooth/task.rs
@@ -1,20 +1,76 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
-use bluez_async::DeviceId;
+use bluez_async::{BluetoothSession, DeviceId, DiscoveryFilter};
+use sqlx::{Pool, Postgres};
+use tokio::sync::broadcast::Sender;
+use tokio::{sync::Mutex, task::JoinHandle};
+use tokio_stream::StreamExt;
 
 use super::parse_bluetooth_advertisement_frame;
 use crate::{
-    drone::Drone,
+    drone::{Drone, DroneBuilder},
     odid::{
         parse_basic_id, parse_location, parse_message_type, parse_operator_id,
         parse_system_message, RemoteIdMessage,
     },
+    web::{insert_drone, DroneDto, DroneUpdate},
 };
 
 pub type MessageType = u8;
 
+pub async fn start_bluetooth_task(
+    device_name: String,
+    drones: Arc<Mutex<HashMap<String, Drone>>>,
+    db_pool: Arc<Mutex<Pool<Postgres>>>,
+    tx: Arc<Mutex<Sender<DroneUpdate>>>,
+) -> JoinHandle<()> {
+    tokio::spawn(async move {
+        let mut drones = drones.lock().await;
+
+        let (_, session) = BluetoothSession::new().await.unwrap();
+        let mut events = session.event_stream().await.unwrap();
+        session
+            .start_discovery_with_filter(&DiscoveryFilter {
+                duplicate_data: Some(true),
+                ..DiscoveryFilter::default()
+            })
+            .await
+            .unwrap();
+
+        println!("Scanning for Bluetooth advertisement data.");
+
+        while let Some(event) = events.next().await {
+            if let Some((device_id, message_type)) =
+                handle_bluetooth_event(&mut drones, device_name.as_str(), event).await
+            {
+                let device_id = device_id.to_string();
+                let drone = drones.get_mut(&device_id);
+
+                if drone.is_some() {
+                    let drone = drone.unwrap();
+                    if drone.payload_ready() {
+                        let drone_dto = DroneDto::from(drone.clone());
+
+                        let db_pool = db_pool.lock().await;
+                        let tx = tx.lock().await;
+
+                        if !drone.is_in_db {
+                            let inserted_drone = insert_drone(drone_dto, &db_pool, &tx).await;
+                            drone.set_in_db(true, inserted_drone.id);
+                        } else {
+                            if message_type == 2 || message_type == 4 {
+                                insert_drone(drone_dto, &db_pool, &tx).await;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
 pub async fn handle_bluetooth_event(
-    drones: &mut HashMap<DeviceId, Drone>,
+    drones: &mut HashMap<String, Drone>,
     device_name: &str,
     event: bluez_async::BluetoothEvent,
 ) -> Option<(DeviceId, MessageType)> {
@@ -31,14 +87,12 @@ pub async fn handle_bluetooth_event(
                         return Some((id, 69));
                     }
 
-                    let drone = drones.get(&id);
+                    let drone = drones.get(&id.to_string());
 
                     if drone.is_none() {
-                        let drone = Drone::new(None, None, None, None);
-                        drones.insert(id.clone(), drone);
+                        let drone = DroneBuilder::default().build().unwrap();
+                        drones.insert(id.to_string(), drone);
                     }
-
-                    let clone_data = data.clone();
 
                     if let Ok((_, bt_advertisement_frame)) =
                         parse_bluetooth_advertisement_frame(data)
@@ -49,31 +103,33 @@ pub async fn handle_bluetooth_event(
                                     if let Ok((_, system_message)) =
                                         parse_system_message(&bt_advertisement_frame.message)
                                     {
-                                        drones
-                                            .get_mut(&id)
-                                            .unwrap()
-                                            .update_system_message(system_message);
+                                        let drone = drones.get_mut(&id.to_string()).unwrap();
+
+                                        drone.update_system_message(system_message);
                                     }
                                 }
                                 RemoteIdMessage::BasicId => {
                                     if let Ok((_, basic_id)) =
                                         parse_basic_id(&bt_advertisement_frame.message)
                                     {
-                                        drones.get_mut(&id).unwrap().update_basic_id(basic_id);
+                                        let drone = drones.get_mut(&id.to_string()).unwrap();
+                                        drone.update_basic_id(basic_id);
                                     }
                                 }
                                 RemoteIdMessage::Location => {
                                     if let Ok((_, location)) =
                                         parse_location(&bt_advertisement_frame.message)
                                     {
-                                        drones.get_mut(&id).unwrap().update_location(location);
+                                        let drone = drones.get_mut(&id.to_string()).unwrap();
+                                        drone.update_location(location);
                                     }
                                 }
                                 RemoteIdMessage::OperatorId => {
                                     if let Ok((_, operator)) =
                                         parse_operator_id(&bt_advertisement_frame.message)
                                     {
-                                        drones.get_mut(&id).unwrap().update_operator(operator);
+                                        let drone = drones.get_mut(&id.to_string()).unwrap();
+                                        drone.update_operator(operator);
                                     }
                                 }
                                 _ => {
